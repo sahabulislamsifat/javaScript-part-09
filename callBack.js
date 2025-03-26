1ee7ba492184c4c7fc79e3b52683f97531ca8beb
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-27 02:24:51 +0600

    Asynchronous JavaScript - 2

--- callBack.js
+++ callBack.js
@@ -33,12 +33,39 @@
 
 // const result = calculate(5, 5, display);
 
-function calculate(num1, num2, callback) {
-  let sum = num1 + num2;
-  if (callback) callback(sum);
-  return sum;
-}
+// function calculate(num1, num2, callback) {
+//   let sum = num1 + num2;
+//   if (callback) callback(sum);
+//   return sum;
+// }
 
-calculate(5, 5, function (result) {
-  console.log(result);
-});
+// calculate(5, 5, function (result) {
+//   console.log(result);
+// });
+
+// part 2
+// console.log("Code line 01");
+// console.log("Code line 02");
+// console.log("Code line 03");
+
+// function hello() {
+//   console.log("Code line 02");
+// }
+// console.log("Code line 01");
+// hello();
+// console.log("Code line 03");
+
+// console.log("line One");
+
+// setTimeout(function () {
+//   console.log("line two");
+// }, 2000);
+
+// console.log("line three");
+// console.log("line One");
+
+// setInterval(function () {
+//   console.log("line two");
+// }, 2000);
+
+// console.log("line three");
